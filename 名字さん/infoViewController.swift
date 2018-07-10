50091f6a35ce9fdeb062bc89c13a840a949a73c6
秀和 <hidekazu5200@gmail.com>
2018-07-10 16:02:30 +0800

    info追加

--- 名字さん/infoViewController.swift
+++ 名字さん/infoViewController.swift
@@ -12,7 +12,12 @@ class infoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // background image
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        bg.image = UIImage(named: "sky.png")
+        bg.layer.zPosition = -1
+        bg.backgroundColor = UIColor.white
+        self.view.addSubview(bg)
         // Do any additional setup after loading the view.
     }
 
